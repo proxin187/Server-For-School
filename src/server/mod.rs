a6c95b8bea89d99e9bedfaf049afc52284f2a177
proxin187 <thugshaker627@gmail.com>
2024-11-07 13:02:51 +0100

    feat: added support for multi chat

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,26 +1,37 @@
 use std::net::{TcpListener, TcpStream, SocketAddr};
-use std::io::Read;
+use std::sync::{Arc, Mutex};
+use std::io::{Read, Write};
 use std::thread;
+
+macro_rules! lock {
+    ($mutex:expr) => {
+        $mutex.lock().map_err(|_| Into::<Box<dyn std::error::Error + Send + Sync>>::into("failed to lock"))
+    }
+}
 
 
 pub struct Client {
+    clients: Arc<Mutex<Vec<TcpStream>>>,
     stream: TcpStream,
     disconnect: bool,
 }
 
 impl Client {
-    pub fn new(stream: TcpStream) -> Client {
-        Client {
+    pub fn new(stream: TcpStream, clients: Arc<Mutex<Vec<TcpStream>>>) -> Result<Client, Box<dyn std::error::Error + Send + Sync>> {
+        lock!(clients)?.push(stream.try_clone()?);
+
+        Ok(Client {
+            clients,
             stream,
             disconnect: false,
-        }
+        })
     }
 
     fn addr(&mut self) -> Result<SocketAddr, Box<dyn std::error::Error + Send + Sync>> {
         self.stream.peer_addr().map_err(|err| err.into())
     }
 
-    pub fn read(&mut self) -> Result<Option<Vec<u8>>, Box<dyn std::error::Error + Send + Sync>> {
+    fn read(&mut self) -> Result<Option<Vec<u8>>, Box<dyn std::error::Error + Send + Sync>> {
         let mut buffer = [0u8; 128];
 
         match self.stream.read(&mut buffer) {
@@ -29,11 +40,30 @@ impl Client {
         }
     }
 
+    fn broadcast(&mut self, bytes: &[u8]) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+        for stream in lock!(self.clients)?.iter_mut() {
+            stream.write_all(bytes)?;
+        }
+
+        Ok(())
+    }
+
+    fn find(&mut self, addr: SocketAddr) -> Result<Option<usize>, Box<dyn std::error::Error + Send + Sync>> {
+        let index = lock!(self.clients)?.iter()
+            .enumerate()
+            .find(|(_, stream)| stream.peer_addr().map_err(|_| ()) == Ok(addr))
+            .map(|(idx, _)| idx);
+
+        Ok(index)
+    }
+
     pub fn handle(&mut self) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         while !self.disconnect {
             match self.read()? {
                 Some(bytes) => {
-                    println!("[INFO] {:?} -> [{:?}, {:?}]", self.addr()?, bytes.clone(), String::from_utf8(bytes));
+                    println!("[INFO] {:?} -> [{:?}, {:?}]", self.addr()?, bytes.clone(), String::from_utf8(bytes.clone()));
+
+                    self.broadcast(&bytes)?;
                 },
                 None => self.disconnect = true,
             }
@@ -41,18 +71,26 @@ impl Client {
 
         println!("[INFO] client disconnected: {:?}", self.addr()?);
 
+        let addr = self.addr()?;
+
+        if let Some(index) = self.find(addr)? {
+            lock!(self.clients)?.remove(index);
+        }
+
         Ok(())
     }
 }
 
 pub struct Listener {
     listener: TcpListener,
+    clients: Arc<Mutex<Vec<TcpStream>>>,
 }
 
 impl Listener {
     pub fn new(bind: &str) -> Result<Listener, Box<dyn std::error::Error + Send + Sync>> {
         Ok(Listener {
             listener: TcpListener::bind(bind)?,
+            clients: Arc::new(Mutex::new(Vec::new())),
         })
     }
 
@@ -64,11 +102,9 @@ impl Listener {
 
             println!("[INFO] client connected: {}", stream.peer_addr()?);
 
-            thread::spawn(move || {
-                let mut client = Client::new(stream);
+            let mut client = Client::new(stream, self.clients.clone())?;
 
-                client.handle()
-            });
+            thread::spawn(move || client.handle());
         }
 
         Ok(())
